a292a224a24953af60af3434c684421f01396e0c
Gabriel <gab.atienza@gmail.com>
2020-05-19 18:30:13 -0700

    add jwt required and optional middleware

--- server/app.py
+++ server/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, Response, request, jsonify
+from flask import Flask, request, jsonify
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
 from models.User import User
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
+from flask_jwt_extended import (
+  JWTManager, 
+  create_access_token, 
+  get_jwt_identity,
+  jwt_required,
+  jwt_optional
+)
 
 
 app = Flask(__name__)
-app.config['JWT_SERET_KEY'] = 'team-bluejay' # to change later
+app.config['JWT_SECRET_KEY'] = 'team-bluejay' # to change later
 jwt = JWTManager(app)
 
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
 
-# Register/Signup Route
+# User Register/Signup Route
 @app.route('/signup', methods=['POST'])
 def create_user():
   """Handle user signup"""
@@ -22,29 +28,23 @@ def create_user():
 
   """ early check if password is less than 6 """
   if len(req_body.password) < 6:
-    return Response({
-      "message": "Password needs to be at least 6 characters"}, 
-      status=400, 
-      mimetype='application/json'
-    )
+    return jsonify(message="Password needs to be at least 6 characters"), 400
   
   user = User.signup(
-      email=req_body.email,
-      password=req_body.password
-    )    
+    email=req_body.email,
+    password=req_body.password
+  )    
 
   if user:
     user_object = user.generate_user_object()
-    message = jsonify(
+    return jsonify(
       message="Succesfully signed up. User created.",
       access_token=create_access_token(identity=user_object)
-    )
-    return Response(message, status=201, mimetype='application/json')
+    ), 201
   else:
-    message = jsonify(message="error signing up")
-    return Response(message, status=400, mimetype='application/json')
+    return jsonify(message="error signing up"), 400
 
-# Login Route
+# User Login Route
 @app.route('/login', methods=['POST'])
 def login_user(): 
   """Handle user login"""
@@ -58,37 +58,55 @@ def login_user():
 
   if user:
     user_object = user.generate_user_object()
-    message = jsonify(
+    return jsonify(
       message="User authenticated.",
       access_token=create_access_token(identity=user_object)
-    )
-    return Response(message, status=200, mimetype='application/json')
+    ), 200
   else:
-    message = jsonify(message="Not authenticated. Please try again.")
-    return Response(message, status=400, mimetype='application/json')
+    # message = jsonify(message="Not authenticated. Please try again.")
+    # return Response(message, status=400, mimetype='application/json')
+    return jsonify(message="Not authenticated. Please try again."), 400
 
-# protected route
-@app.route('/protected', methods=['GET'])
+
+# Chef: register as chef aroute
+@app.route('/register_chef')
 @jwt_required
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+def register_chef():
+  current_user = get_jwt_identity()
+  if not current_user:
+    return jsonify(message="Unauthorized"), 400
+  
+  # to implement later on
+  # chef = Chef.register()
 
-if __name__ == '__main__':
-    app.run()
-
-@app.route('/partially-protected', methods=['GET'])
-@jwt_optional
-def partially_protected():
-    # If no JWT is sent in with the request, get_jwt_identity()
-    # will return None
-    current_user = get_jwt_identity()
-    if current_user:
-        return jsonify(logged_in_as=current_user), 200
-    else:
-        return jsonify(logged_in_as='anonymous user'), 200
+  # if chef:
+  #   chef_object = chef.generate_chef_object()
+  #   return jsonify(
+  #     message="Congratulations! You are now a chef.",
+  #     access_token=create_access_token(identity=user_object)
+  #   ), 200
+  # else:
+  #   return jsonify(message="Registration as a chef failed"), 400
+  return
 
 
-if __name__ == '__main__':
-    app.run()
+
+
+# ================================
+# sample partially protected route
+# ================================
+
+# @app.route('/<some route>', methods=['GET'])
+# @jwt_optional
+# def partially_protected():
+#     # If no JWT is sent in with the request, get_jwt_identity()
+#     # will return None
+#     current_user = get_jwt_identity()
+#     if current_user:
+#         return jsonify(logged_in_as=current_user), 200
+#     else:
+#         return jsonify(logged_in_as='anonymous user'), 200
+
+
+# if __name__ == '__main__':
+#     app.run()
