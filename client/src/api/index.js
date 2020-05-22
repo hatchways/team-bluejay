b286b4e5c65e726a1d99f2ecaa3746713f46d469
kareem sakr <kareem.h.sakr@gmail.com>
2020-05-22 13:31:11 -0400

    removed uncessary commented code

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,13 +4,4 @@ const instance = axios.create({
   baseURL: process.env.API_URL,
 });
 
-// TODO
-// instance.interceptors.request.use(async config => {
-//   const token = get token from cookies (might not need to do it manually)
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
 export default instance;
