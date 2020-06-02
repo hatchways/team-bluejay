19cd8420e29f22e87be922703cd4beece02bdbf4
Moshe Siegel <siegel.moshes@gmail.com>
2020-06-02 18:51:16 -0400

    Added migrations

--- server/models/User.py
+++ server/models/User.py
@@ -1,5 +1,40 @@
 from . import db, bcrypt
-from marshmallow import fields, Schema, validate, validates_schema, ValidationError
+from marshmallow import fields, Schema, validate, validates, validates_schema, ValidationError, pre_dump
+
+favorite_cuisines_table = db.Table('favorite_cuisines',
+                                   db.Column('user_id', db.Integer,
+                                             db.ForeignKey('users.id'), primary_key=True),
+                                   db.Column('cuisine_id', db.Integer,
+                                             db.ForeignKey('cuisines.id'), primary_key=True)
+                                   )
+
+
+class Cuisine(db.Model):
+    __tablename__ = 'cuisines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False, unique=True)
+    users = db.relationship('User',
+                            secondary=favorite_cuisines_table,
+                            back_populates='cuisines'
+                            )
+
+    def __init__(self, name):
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    # filter my multiple ids
+    def get_cuisine_by_name(value):
+        return Cuisine.query.filter_by(name=value).first()
+
+
+class CuisineSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
 
 
 class User(db.Model):
@@ -19,9 +54,13 @@ class User(db.Model):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     formattedAddress = db.Column(db.String)
+    cuisines = db.relationship('Cuisine',
+                               secondary=favorite_cuisines_table,
+                               back_populates='users'
+                               )
 
     # Todo: this method of initializing with default values feels very sloppy and a better way to do it probably exists
-    def __init__(self, name, email, password, confirmPassword, aboutMe="", streetAddress="", city="", state="", zipcode="", country="", latitude=0.0, longitude=0.0, formattedAddress=""):
+    def __init__(self, name, email, password, confirmPassword, aboutMe="", streetAddress="", city="", state="", zipcode="", country="", latitude=0.0, longitude=0.0, formattedAddress="", cuisines=[]):
         self.name = name
         self.email = email
         self.password = self.__generate_hash(password)
@@ -35,6 +74,13 @@ class User(db.Model):
         self.latitude = latitude
         self.longitude = longitude
         self.formattedAddress = formattedAddress
+        if (len(cuisines) > 0):
+            updated_cuisines = []
+            for cuisine in cuisines:
+                # Should be:  target_cuisine = Cuisine.get_cuisine_by_id(cuisine)
+                target_cuisine = Cuisine.get_cuisine_by_name(cuisine)
+                updated_cuisines.append(target_cuisine)
+            self.cuisines = updated_cuisines
 
     def __repr__(self):
         return f"<User #{self.id}: {self.name}, {self.email}>"
@@ -97,9 +143,22 @@ class UserSchema(Schema):
     latitude = fields.Float()
     longitude = fields.Float()
     formattedAddress = fields.String()
+    #cuisines = fields.List(fields.Nested(CuisineSchema))
+    cuisines = fields.List(fields.String())
 
     @validates_schema
     def validate_password(self, data, **kwargs):
         if (data.get("password") and data.get("confirmPassword")) and (data.get("password") != data.get("confirmPassword")):
             # For all validation errors, Marshmallow raises its own error type called ValidationError
             raise ValidationError("Passwords do not match")
+
+    @validates("cuisines")
+    def validates_cuisines(self, cuisines):
+        if (len(cuisines) > 0):
+            # for database calls rarely a need to do a for loop
+            for cuisine in cuisines:
+                print(cuisine)
+                # get all cuisines and if number of returned is different than return ValidationError
+                if Cuisine.get_cuisine_by_name(cuisine) == None:
+                    raise ValidationError(
+                        cuisine + " is not a valid cuisine registered within our database")
