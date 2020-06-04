4d6c7d236be98c340f1e12d99eaaa61e0fec5fe2
Gabriel <gab.atienza@gmail.com>
2020-06-03 20:23:58 -0700

    remove key, add .env to gitignore, cleanup from comments

--- server/helpers/google.py
+++ server/helpers/google.py
@@ -7,8 +7,6 @@ def address_to_data(address):
     request_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={os.environ.get("GOOGLE_API_KEY")}'
     try:
         data = requests.get(request_url).json().get("results")[0]
-    except:
+    except Exception:
         return
-    # print(data.get("access_points")[0].get("location"))
-    # print(data.get("formatted_address"))
     return data
