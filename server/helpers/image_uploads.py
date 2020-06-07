8fbc56bd70d9ee612111c64dc2a29d9323719fd2
Moshe Siegel <siegel.moshes@gmail.com>
2020-06-06 23:02:10 -0400

    removed unnecessary server images folder

--- server/helpers/image_uploads.py
+++ server/helpers/image_uploads.py
@@ -8,7 +8,6 @@ def upload_profile_picture(file, profile_id):
     if not is_image_file(file.filename):
         return False
 
-    local_file = 'images/sample_image.png'
     bucket = 'team-bluejay'
     bucket_folder = 'profile_pictures'
     s3_file_path = f'{bucket_folder}/UserID-{str(profile_id)}.jpg'
@@ -19,8 +18,8 @@ def upload_profile_picture(file, profile_id):
     # Todo: put hosting location of imageUrl into config file and read from there, "S3_LOCATION"
     imageUrl = f'https://team-bluejay.s3.amazonaws.com/{s3_file_path}'
     try:
-        s3.upload_file(local_file, bucket, s3_file_path,
-                       ExtraArgs={'ACL': 'public-read', "ContentType": file.content_type})
+        s3.upload_fileobj(file, bucket, s3_file_path,
+                          ExtraArgs={'ACL': 'public-read', "ContentType": file.content_type})
     except Exception as e:
         print(e)
         return False
