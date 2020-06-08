4e246b3fbf5303091b0e1c52324ae34b5e0821bc
Moshe Siegel <siegel.moshes@gmail.com>
2020-06-08 11:14:47 -0400

    Create DialogContext.js

---
+++ client/src/contexts/DialogContext.js
@@ -0,0 +1,61 @@
+// How Dialog Context is Used:
+
+// Profile.js
+const { openDialog } = useContext(dialogContext);
+// edit profile button withing Profile.js
+<Button onClick={() => openDialog(<EditProfileButton />)} />;
+
+//EditProfileForm.js
+const { closeDialog } = useContext(dialogContext);
+// function that runs on form submit
+const submitForm = (profileData) => {
+  updateUser(profileData);
+  closeDialog();
+};
+
+//DialogContext
+
+import React, { useState } from "react";
+
+import CloseIcon from "@material-ui/icons/Close";
+import { Dialog, DialogActions, DialogContent } from "@material-ui/core";
+
+const Context = React.createContext();
+
+const Provider = ({ children }) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [StuffInsideDialogue, setStuffInsideDialog] = useState(null);
+
+  function openDialog(componentWithin) {
+    setStuffInsideDialog(componentWithin);
+    setDialogOpen(true);
+  }
+
+  function closeDialog() {
+    setStuffInsideDialog(null);
+    setDialogOpen(false);
+  }
+
+  return (
+    <Context.Provider value={{ openDialog, closeDialog }}>
+      <Dialog
+        fullWidth
+        maxWidth="md"
+        open={dialogOpen}
+        aria-labelledby="max-width-dialog-title"
+        color="primary"
+      >
+        <DialogActions>
+          <CloseIcon onClick={closeDialog} color="primary" />
+        </DialogActions>
+        <DialogContent>
+          {/* Problem:  Throws error that useEffect and useState hooks from StuffInsideDialogue must be called on top level of component*/}
+          {StuffInsideDialogue && <StuffInsideDialogue />}
+        </DialogContent>
+      </Dialog>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export { Provider, Context };
