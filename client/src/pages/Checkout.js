b3de9436ef9fc95b61d51b9c4759ab226ccd6839
Gabriel <gab.atienza@gmail.com>
2020-06-07 18:56:36 -0700

    orders list styling

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -16,9 +16,11 @@ import {
   Button,
   FormHelperText,
 } from "@material-ui/core";
+import { AddCircle, RemoveCircle } from "@material-ui/icons";
 import PaymentForm from "components/PaymentForm";
 import { makeStyles } from "@material-ui/core/styles";
 import { Context as MealContext } from "contexts/MealContext";
+import foodImg from "images/makisushi.jpg";
 
 const Checkout = () => {
   const classes = useStyles();
@@ -29,20 +31,14 @@ const Checkout = () => {
   return (
     <div className={classes.root}>
       <Grid container>
-        <Grid item xs={12} sm={12} md={8} lg={9}>
+        <Grid item xs={12} sm={12} md={8} lg={8}>
           <Paper className={classes.checkoutPaper} elevation={0}>
             <Box className={classes.checkoutBox}>
               <Typography variant="h4" component="h1">
                 Checkout
               </Typography>
             </Box>
-            <hr
-              style={{
-                borderWidth: "0.25px",
-                color: "red",
-                backgroundColor: "red",
-              }}
-            />
+            <hr />
             <Box className={classes.checkoutBox}>
               <Typography variant="h6" component="h3">
                 Enter your payment details
@@ -51,18 +47,25 @@ const Checkout = () => {
             </Box>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={12} md={4} lg={3}>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
           <Paper className={classes.checkoutPaper} elevation={2}>
-            <Box>
-              <Typography variant="h6" align="center">
+            <Box className={classes.yourOrdersHeader}>
+              <Typography variant="h5" align="center">
                 Your Order
               </Typography>
             </Box>
             <Box>
               {shoppingCart.map((item) => (
-                <Card>{item.name + ": " + item.quantity}</Card>
+                <OrderCard key={item.id} mealItem={item} />
               ))}
             </Box>
+
+            <Typography variant="h4" display="inline">
+              Total :
+            </Typography>
+            <Typography variant="h4" display="inline">
+              $ 90
+            </Typography>
           </Paper>
         </Grid>
       </Grid>
@@ -70,11 +73,67 @@ const Checkout = () => {
   );
 };
 
+const OrderCard = ({ mealItem }) => {
+  const classes = useStyles();
+  const { name, id, quantity, price } = mealItem;
+  return (
+    <Card className={classes.mealItemCard}>
+      <CardMedia className={classes.mealItemPic} image={foodImg} title={name} />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="body1">{name}</Typography>
+        <Typography variant="h6">{`$${price} x ${quantity}`}</Typography>
+        <AddCircle />
+        <RemoveCircle />
+      </CardContent>
+    </Card>
+  );
+};
+
 const useStyles = makeStyles((theme) => ({
+  cardContent: {
+    color: theme.palette.secondary.main,
+    "& p": {
+      marginBottom: theme.spacing(1),
+    },
+    "& svg": {
+      marginLeft: theme.spacing(1),
+      marginTop: theme.spacing(1),
+    },
+    "& svg:nth-of-type(1)": {
+      color: theme.palette.primary.main,
+    },
+  },
+  mealItemPic: {
+    minHeight: "100px",
+    minWidth: "100px",
+    maxWidth: "40%",
+  },
+  mealItemCard: {
+    padding: theme.spacing(2),
+    display: "flex",
+  },
+  yourOrdersHeader: {
+    padding: theme.spacing(3),
+    fontWeight: "bold",
+    borderBottom: `1px solid ${theme.palette.primary.main}`,
+  },
   checkoutBox: {
     padding: theme.spacing(3, 6, 3, 6),
   },
-  checkoutPaper: { margin: theme.spacing(0, 2, 0, 2) },
+  checkoutPaper: {
+    margin: theme.spacing(0, 2, 0, 2),
+    "& hr": {
+      border: `1px solid ${theme.palette.primary.main}`,
+    },
+    "& h4": {
+      margin: theme.spacing(3, 2, 3, 2),
+      lineHeight: "3em",
+      color: theme.palette.primary.main,
+    },
+    "& h4:nth-child(2)": {
+      color: theme.palette.primary.main,
+    },
+  },
   root: {
     marginTop: theme.spacing(1),
   },
