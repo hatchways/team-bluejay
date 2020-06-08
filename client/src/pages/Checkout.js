444d4b70b1b5f2a48437c6dce28e645b7c3d987b
Gabriel <gab.atienza@gmail.com>
2020-06-07 17:34:50 -0700

    finish paymentform

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -16,6 +16,7 @@ import {
   Button,
   FormHelperText,
 } from "@material-ui/core";
+import PaymentForm from "components/PaymentForm";
 import { makeStyles } from "@material-ui/core/styles";
 import { Context as MealContext } from "contexts/MealContext";
 
@@ -25,10 +26,55 @@ const Checkout = () => {
     state: { shoppingCart },
   } = useContext(MealContext);
 
-  return <div>{shoppingCart.length}</div>;
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={8} lg={9}>
+          <Paper className={classes.checkoutPaper} elevation={0}>
+            <Box className={classes.checkoutBox}>
+              <Typography variant="h4" component="h1">
+                Checkout
+              </Typography>
+            </Box>
+            <hr
+              style={{
+                borderWidth: "0.25px",
+                color: "red",
+                backgroundColor: "red",
+              }}
+            />
+            <Box className={classes.checkoutBox}>
+              <Typography variant="h6" component="h3">
+                Enter your payment details
+              </Typography>
+              <PaymentForm />
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          <Paper className={classes.checkoutPaper} elevation={2}>
+            <Box>
+              <Typography variant="h6" align="center">
+                Your Order
+              </Typography>
+            </Box>
+            <Box>
+              {shoppingCart.map((item) => (
+                <Card>{item.name + ": " + item.quantity}</Card>
+              ))}
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
+  checkoutBox: {
+    padding: theme.spacing(3, 6, 3, 6),
+  },
+  checkoutPaper: { margin: theme.spacing(0, 2, 0, 2) },
   root: {
     marginTop: theme.spacing(1),
   },
