72d510e831bc7905eec2073653270af932d82121
Gabriel <gab.atienza@gmail.com>
2020-06-11 20:48:30 -0700

    get order details route

--- server/api/order_handler.py
+++ server/api/order_handler.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource
 from flask import jsonify, request, json
-from flask_jwt_extended import jwt_required 
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.Order import Order, OrderSchema
 from helpers.api import custom_json_response
 
 order_schema = OrderSchema()
+
 
 class OrderResource(Resource):
     @jwt_required
@@ -14,7 +15,7 @@ class OrderResource(Resource):
         order.fulfill(req_body.get("clientSecret"))
         return custom_json_response({"message": "order created"}, 201)
 
+    @jwt_required
     def get(self, id):
         order = Order.get_by_id(id)
-        
         return order_schema.dump(order)
