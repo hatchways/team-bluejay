d80b2027ca812b3e5cd44ce3cb8ebe0471151d1c
Moshe Siegel <siegel.moshes@gmail.com>
2020-06-26 11:50:11 -0400

    added more to app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,26 @@
-from flask import Flask
+from flask import Flask, json, jsonify, request
+from marshmallow import Schema
+from flask_restful import Api
+from config import DB_URL
+from socket_events import socketio
+
+from api.login_handler import LoginResource
+from api.user_handler import UserResource
+from api.chef_handler import ChefResource
+from api.meal_item_handler import MealItemResource
+from api.LogoutResource import LogoutResource
+from api.StripeResource import StripeResource
+from api.cuisine_handler import CuisineResource
+from api.notification_handler import NotificationResource
+from api.order_handler import OrderResource
+
+
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity
+)
+from models import db
 
 
 def create_app():
