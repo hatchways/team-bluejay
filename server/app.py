ba1677d70eeef42108d144c37c8127b7afac9855
Moshe Siegel <siegel.moshes@gmail.com>
2020-06-26 12:55:29 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,5 +14,5 @@ def create_app():
     return app
 
 
-# if __name__ == "__main__" or os.getenv('FLASK_ENV') == 'production':
-app = create_app()
+if __name__ == "__main__" or os.getenv('FLASK_ENV') == 'production':
+    app = create_app()
