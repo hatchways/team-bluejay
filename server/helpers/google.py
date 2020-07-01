baf6af0d8586e6e06abcc546e8dfa00585436b41
Moshe Siegel <siegel.moshes@gmail.com>
2020-07-01 16:08:15 -0400

    adding logging messages for debugging

--- server/helpers/google.py
+++ server/helpers/google.py
@@ -4,8 +4,12 @@ import requests
 
 def address_to_data(address):
     request_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={os.environ.get("GOOGLE_API_KEY")}'
+    print(address)
     try:
+        print("ABOUT TO PRINT REQUEST DATA TO GOOGLE")
         data = requests.get(request_url).json().get("results")[0]
-    except Exception:
+        print(data)
+    except Exception as e:
+        print(e)
         return
     return data
