9844a2904387c921109dfcb499c149c62f7c34c4
Moshe Siegel <siegel.moshes@gmail.com>
2020-07-01 16:40:38 -0400

    added missing socketio.run(app)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from server import create_app
+from server import create_app, socketio
 import os
 
 if __name__ == "__main__" or os.getenv('FLASK_ENV') == 'production':
     app = create_app()
+    socketio.run(app)
