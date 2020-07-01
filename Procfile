c6fa75c310bde3d6cf46485e7711177330927463
Moshe Siegel <siegel.moshes@gmail.com>
2020-07-01 10:40:01 -0400

    allow all socket origins

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class eventlet -w 1 app:app && pip install eventlet
+web: gunicorn --worker-class eventlet -w 1 app:app
--- server/config.py
+++ server/config.py
@@ -10,5 +10,5 @@ DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
-ALLOWED_CLIENT_URLS = ['http://localhost:5000',
-                       'http://localhost:3000', 'https://team-bluejay.herokuapp.com/']
+ALLOWED_CLIENT_URLS = '*'  # ['http://localhost:5000',
+# 'http://localhost:3000', 'https://team-bluejay.herokuapp.com/']
