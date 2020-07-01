05fb7de80dd4cb3ecbe43d32b3115bf3a0692d69
Moshe Siegel <siegel.moshes@gmail.com>
2020-07-01 16:51:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from server import create_app, socketio
 import os
 
+app = create_app()
+
 if __name__ == "__main__" or os.getenv('FLASK_ENV') == 'production':
-    app = create_app()
     socketio.run(app)
